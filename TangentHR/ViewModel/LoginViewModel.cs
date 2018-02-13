b5f7fa3e54a9e3d286067f0adde41cd9d317b098
Thulani Mtetwa <thulani.mtetwa@roh.com>
2018-02-14 00:52:28 +0200

    login viewmodel set

---
+++ TangentHR/ViewModel/LoginViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TangentHR.ViewModel
+{
+    public class LoginViewModel: INotifyPropertyChanged
+    {
+
+        string username, password;
+        public LoginViewModel()
+        {
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                if (username != value)
+                {
+                    username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public void SetUsername()
+        {
+            System.Diagnostics.Debug.WriteLine(username);
+        }
+    }
+}
--- TangentHR/Views/LoginPage.xaml.cs
+++ TangentHR/Views/LoginPage.xaml.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using TangentHR.ViewModel;
 using Xamarin.Forms;
 
 namespace TangentHR.Views
 {
     public partial class LoginPage : ContentPage
     {
+        LoginViewModel login;
         public LoginPage()
         {
             InitializeComponent();
+            login = new LoginViewModel();
+            BindingContext = login;
+        }
+
+        void SignIn(object sender, System.EventArgs e)
+        {
+            login.SetUsername();
         }
     }
 }
